1c8ab4a5f587c7877e82043ac31206ede2191a9b
Paola Silva <paolavic014@outlook.com>
2024-08-22 08:50:44 -0300

    corrigindo os aplications

--- src/main/java/com/generation/blogpessoal/security/JwtService.java
+++ src/main/java/com/generation/blogpessoal/security/JwtService.java
@@ -18,7 +18,7 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtService {
 
-	public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+	public static final String SECRET = "f54b7ebc6dc24d2c04fc144514d7c7d7bb1bf0935915c9bf813d28416358fa46";
 
 	private Key getSignKey() {
 		byte[] keyBytes = Decoders.BASE64.decode(SECRET);
